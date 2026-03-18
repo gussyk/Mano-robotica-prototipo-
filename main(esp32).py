8195dc3853172f82c41705607dc867a6cf6c3fe0
Guss{/} <ytgussy@gmail.com>
2026-03-17 23:52:33 -0600

    Add files via upload

---
+++ main(esp32).py
@@ -0,0 +1,36 @@
+from machine import Pin, PWM
+from time import sleep
+
+
+
+
+
+
+servo1 = PWM(Pin(17), freq=50)#indice
+servo2 = PWM(Pin(26), freq=50)
+servo3 = PWM(Pin(12), freq=50)
+servo4 = PWM(Pin(14), freq=50)
+servo5 = PWM(Pin(15), freq=50)
+
+while True:
+    lista = input("ingrese la lista")
+    if lista[0] == "1":
+        servo1.duty_ns(2400000)
+    else:
+        servo1.duty_ns(544000)
+    if lista[1] == "1":
+        servo2.duty_ns(2400000)
+    else:
+        servo2.duty_ns(544000)
+    if lista[2] == "1":
+        servo3.duty_ns(2400000)
+    else:
+        servo3.duty_ns(544000)
+    if lista[3] == "1":
+        servo4.duty_ns(2400000)
+    else:
+        servo4.duty_ns(544000)
+    if lista[4] == "1":
+        servo5.duty_ns(2400000)
+    else:
+        servo5.duty_ns(544000)
